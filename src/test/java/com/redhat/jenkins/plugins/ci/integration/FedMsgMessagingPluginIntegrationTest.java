5aa04d8e9ba73ab1ba66f5a32846c92f2fbb8ab5
Scott Hebert <shebert@redhat.com>
2016-12-20 11:44:28 -0500

    FedMsg Message Provider
    - supports triggering
    - supports sending and receiving
    
    Signing of messages still not implemented.
    
    - Updated Jenkinsfile to use docker.build()

--- src/test/java/com/redhat/jenkins/plugins/ci/integration/FedMsgMessagingPluginIntegrationTest.java
+++ src/test/java/com/redhat/jenkins/plugins/ci/integration/FedMsgMessagingPluginIntegrationTest.java
@@ -1,13 +1,12 @@
 package com.redhat.jenkins.plugins.ci.integration;
 
-import com.google.inject.Inject;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.jenkinsci.test.acceptance.Matchers.hasContent;
+
 import com.redhat.jenkins.plugins.ci.integration.docker.fixtures
-        .JBossAMQContainer;
-import com.redhat.jenkins.plugins.ci.integration.po.ActiveMqMessagingProvider;
-import com.redhat.jenkins.plugins.ci.integration.po.CIEventTrigger;
-import com.redhat.jenkins.plugins.ci.integration.po.CINotifierPostBuildStep;
-import com.redhat.jenkins.plugins.ci.integration.po.CISubscriberBuildStep;
-import com.redhat.jenkins.plugins.ci.integration.po.GlobalCIConfiguration;
+        .FedmsgRelayContainer;
+import com.redhat.jenkins.plugins.ci.integration.po.FedMsgMessagingProvider;
 import org.jenkinsci.test.acceptance.docker.DockerContainerHolder;
 import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
 import org.jenkinsci.test.acceptance.junit.WithDocker;
@@ -18,9 +17,11 @@ import org.jenkinsci.test.acceptance.po.WorkflowJob;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.jenkinsci.test.acceptance.Matchers.hasContent;
+import com.google.inject.Inject;
+import com.redhat.jenkins.plugins.ci.integration.po.CIEventTrigger;
+import com.redhat.jenkins.plugins.ci.integration.po.CINotifierPostBuildStep;
+import com.redhat.jenkins.plugins.ci.integration.po.CISubscriberBuildStep;
+import com.redhat.jenkins.plugins.ci.integration.po.GlobalCIConfiguration;
 
 /*
  * The MIT License
@@ -47,157 +48,21 @@ import static org.jenkinsci.test.acceptance.Matchers.hasContent;
  */
 @WithPlugins("jms-messaging")
 @WithDocker
-public class AmqMessagingPluginIntegrationTest extends AbstractJUnitTest {
-    @Inject private DockerContainerHolder<JBossAMQContainer> docker;
+public class FedMsgMessagingPluginIntegrationTest extends AbstractJUnitTest {
+    @Inject private DockerContainerHolder<FedmsgRelayContainer> docker;
 
-    private JBossAMQContainer amq = null;
-    private static final int INIT_WAIT = 360;
+    private FedmsgRelayContainer fedmsgRelay = null;
 
     @Before public void setUp() throws Exception {
-        amq = docker.get();
+        fedmsgRelay = docker.get();
         jenkins.configure();
-        elasticSleep(5000);
         GlobalCIConfiguration ciPluginConfig = new GlobalCIConfiguration(jenkins.getConfigPage());
-        ActiveMqMessagingProvider msgConfig = new ActiveMqMessagingProvider(ciPluginConfig).addMessagingProvider();
+        FedMsgMessagingProvider msgConfig = new FedMsgMessagingProvider(ciPluginConfig).addMessagingProvider();
         msgConfig.name("test")
-            .broker(amq.getBroker())
-            .topic("CI")
-            .user("admin")
-            .password("redhat");
-
-        int counter = 0;
-        boolean connected = false;
-        while (counter < INIT_WAIT) {
-            try {
-                msgConfig.testConnection();
-                waitFor(driver, hasContent("Successfully connected to " + amq.getBroker()), 5);
-                connected = true;
-                break;
-            } catch (Exception e) {
-                counter++;
-                elasticSleep(1000);
-            }
-        }
-        if (!connected) {
-            throw new Exception("Did not get connection successful message in " + INIT_WAIT + " secs.");
-        }
-        elasticSleep(1000);
+            .topic("tom")
+            .hubAddr(fedmsgRelay.getHub())
+            .pubAddr(fedmsgRelay.getPublisher());
         jenkins.save();
-    }
-
-    @Test
-    public void testGlobalConfigTestConnection() throws Exception {
-    }
-
-    @Test
-    public void testAddDuplicateMessageProvider() throws Exception {
-        jenkins.configure();
-        elasticSleep(5000);
-        GlobalCIConfiguration ciPluginConfig = new GlobalCIConfiguration(jenkins.getConfigPage());
-        ActiveMqMessagingProvider msgConfig = new ActiveMqMessagingProvider(ciPluginConfig).addMessagingProvider();
-        msgConfig.name("test")
-                .broker(amq.getBroker())
-                .topic("CI")
-                .user("admin")
-                .password("redhat");
-        jenkins.save();
-        assertThat(driver, hasContent("Attempt to add a duplicate JMS Message Provider - test"));
-    }
-
-    @WithPlugins("workflow-aggregator@1.2")
-    @Test
-    public void testSimpleCIEventTriggerWithPipelineSendMsg() throws Exception {
-        FreeStyleJob jobA = jenkins.jobs.create();
-        jobA.configure();
-        jobA.addShellStep("echo CI_TYPE = $CI_TYPE");
-        CIEventTrigger ciEvent = new CIEventTrigger(jobA);
-        ciEvent.selector.set("CI_TYPE = 'code-quality-checks-done' and CI_STATUS = 'failed'");
-        jobA.save();
-
-        WorkflowJob job = jenkins.jobs.create(WorkflowJob.class);
-        job.script.set("node('master') {\n sendCIMessage " +
-                " providerName: 'test', " +
-                " messageContent: '', " +
-                " messageProperties: 'CI_STATUS = failed'," +
-                " messageType: 'CodeQualityChecksDone'}");
-        job.save();
-        job.startBuild().shouldSucceed();
-
-        elasticSleep(1000);
-        jobA.getLastBuild().shouldSucceed().shouldExist();
-
-    }
-
-    @WithPlugins("workflow-aggregator@1.2")
-    @Test
-    public void testSimpleCIEventTriggerWithPipelineWaitForMsg() throws Exception {
-        WorkflowJob wait = jenkins.jobs.create(WorkflowJob.class);
-        wait.script.set("node('master') {\n def scott = waitForCIMessage  providerName: 'test', " +
-                " selector: " +
-                " \"CI_TYPE = 'code-quality-checks-done' and CI_STATUS = 'failed'\"  \necho \"scott = \" + scott}");
-        wait.save();
-        wait.startBuild();
-
-        FreeStyleJob jobB = jenkins.jobs.create();
-        jobB.configure();
-        CINotifierPostBuildStep notifier = jobB.addPublisher(CINotifierPostBuildStep.class);
-        notifier.messageType.select("CodeQualityChecksDone");
-        notifier.messageProperties.sendKeys("CI_STATUS = failed");
-        notifier.messageContent.set("Hello World");
-        jobB.save();
-        jobB.startBuild().shouldSucceed();
-
-        elasticSleep(1000);
-        wait.getLastBuild().shouldSucceed();
-        assertThat(wait.getLastBuild().getConsole(), containsString("Hello World"));
-    }
-
-    @WithPlugins("workflow-aggregator")
-    @Test
-    public void testSimpleCIEventSendAndWaitPipeline() throws Exception {
-        WorkflowJob wait = jenkins.jobs.create(WorkflowJob.class);
-        wait.script.set("node('master') {\n def scott = waitForCIMessage providerName: 'test'," +
-                "selector: " +
-                " \"CI_TYPE = 'code-quality-checks-done' and CI_STATUS = 'failed'\"  \necho \"scott = \" + scott}");
-        wait.save();
-        wait.startBuild();
-
-        WorkflowJob send = jenkins.jobs.create(WorkflowJob.class);
-        send.script.set("node('master') {\n sendCIMessage" +
-                " providerName: 'test', " +
-                " messageContent: 'abcdefg', " +
-                " messageProperties: 'CI_STATUS = failed'," +
-                " messageType: 'CodeQualityChecksDone'}");
-        send.save();
-        send.startBuild().shouldSucceed();
-        send.getLastBuild().getConsole().contains("scott = abcdefg");
-
-        elasticSleep(1000);
-        wait.getLastBuild().shouldSucceed();
-
-    }
-
-    @Test
-    public void testSimpleCIEventTrigger() throws Exception {
-        FreeStyleJob jobA = jenkins.jobs.create();
-        jobA.configure();
-        jobA.addShellStep("echo CI_TYPE = $CI_TYPE");
-        CIEventTrigger ciEvent = new CIEventTrigger(jobA);
-        ciEvent.selector.set("CI_TYPE = 'code-quality-checks-done' and CI_STATUS = 'failed'");
-        jobA.save();
-        elasticSleep(1000);
-
-        FreeStyleJob jobB = jenkins.jobs.create();
-        jobB.configure();
-        CINotifierPostBuildStep notifier = jobB.addPublisher(CINotifierPostBuildStep.class);
-        notifier.messageType.select("CodeQualityChecksDone");
-        notifier.messageProperties.sendKeys("CI_STATUS = failed");
-        jobB.save();
-        jobB.startBuild().shouldSucceed();
-
-        elasticSleep(1000);
-        jobA.getLastBuild().shouldSucceed().shouldExist();
-        assertThat(jobA.getLastBuild().getConsole(), containsString("echo CI_TYPE = code-quality-checks-done"));
     }
 
     @Test
@@ -224,6 +89,68 @@ public class AmqMessagingPluginIntegrationTest extends AbstractJUnitTest {
         elasticSleep(1000);
         jobA.getLastBuild().shouldSucceed().shouldExist();
         assertThat(jobA.getLastBuild().getConsole(), containsString("Hello World"));
+    }
+
+    @WithPlugins("workflow-aggregator@1.2")
+    @Test
+    public void testSimpleCIEventTriggerWithPipelineSendMsg() throws Exception {
+        FreeStyleJob jobA = jenkins.jobs.create();
+        jobA.configure();
+        jobA.addShellStep("echo CI_TYPE = $CI_TYPE");
+        CIEventTrigger ciEvent = new CIEventTrigger(jobA);
+        ciEvent.selector.set("CI_TYPE = 'code-quality-checks-done' and CI_STATUS = 'failed'");
+        jobA.save();
+
+        WorkflowJob job = jenkins.jobs.create(WorkflowJob.class);
+        job.script.set("node('master') {\n sendCIMessage " +
+                " providerName: 'test', " +
+                " messageContent: '', " +
+                " messageProperties: 'CI_STATUS = failed'," +
+                " messageType: 'CodeQualityChecksDone'}");
+        job.save();
+        job.startBuild().shouldSucceed();
+
+        elasticSleep(1000);
+        jobA.getLastBuild().shouldSucceed().shouldExist();
+
+    }
+
+    @Test
+    public void testAddDuplicateMessageProvider() throws Exception {
+        jenkins.configure();
+        elasticSleep(5000);
+        GlobalCIConfiguration ciPluginConfig = new GlobalCIConfiguration(jenkins.getConfigPage());
+        FedMsgMessagingProvider msgConfig = new FedMsgMessagingProvider(ciPluginConfig).addMessagingProvider();
+        msgConfig.name("test")
+                .topic("tom")
+                .hubAddr("tcp://127.0.0.1:4001")
+                .pubAddr("tcp://127.0.0.1:2003");
+        jenkins.save();
+        assertThat(driver, hasContent("Attempt to add a duplicate JMS Message Provider - test"));
+    }
+
+    @Test
+    public void testSimpleCIEventTrigger() throws Exception {
+        FreeStyleJob jobA = jenkins.jobs.create();
+        jobA.configure();
+        jobA.addShellStep("echo CI_TYPE = $CI_TYPE");
+        CIEventTrigger ciEvent = new CIEventTrigger(jobA);
+        ciEvent.selector.set("CI_TYPE = 'code-quality-checks-done' and CI_STATUS = 'failed'");
+        jobA.save();
+        // Allow for connection
+        elasticSleep(5000);
+
+        FreeStyleJob jobB = jenkins.jobs.create();
+        jobB.configure();
+        CINotifierPostBuildStep notifier = jobB.addPublisher(CINotifierPostBuildStep.class);
+        notifier.messageType.select("CodeQualityChecksDone");
+        notifier.messageProperties.sendKeys("CI_STATUS = failed");
+        jobB.save();
+        jobB.startBuild().shouldSucceed();
+
+        elasticSleep(1000);
+        jobA.getLastBuild().shouldSucceed().shouldExist();
+        assertThat(jobA.getLastBuild().getConsole(), containsString("echo CI_TYPE = code-quality-checks-done"));
     }
 
     @Test
@@ -255,6 +182,55 @@ public class AmqMessagingPluginIntegrationTest extends AbstractJUnitTest {
         jobA.getLastBuild().shouldSucceed().shouldExist();
         assertThat(jobA.getLastBuild().getConsole(), containsString("my parameter"));
         assertThat(jobA.getLastBuild().getConsole(), containsString("This is my content"));
+    }
+
+    @WithPlugins("workflow-aggregator")
+    @Test
+    public void testSimpleCIEventSendAndWaitPipeline() throws Exception {
+        WorkflowJob wait = jenkins.jobs.create(WorkflowJob.class);
+        wait.script.set("node('master') {\n def scott = waitForCIMessage providerName: 'test'," +
+                "selector: " +
+                " \"CI_TYPE = 'code-quality-checks-done' and CI_STATUS = 'failed'\"  \necho \"scott = \" + scott}");
+        wait.save();
+        wait.startBuild();
+
+        WorkflowJob send = jenkins.jobs.create(WorkflowJob.class);
+        send.script.set("node('master') {\n sendCIMessage" +
+                " providerName: 'test', " +
+                " messageContent: 'abcdefg', " +
+                " messageProperties: 'CI_STATUS = failed'," +
+                " messageType: 'CodeQualityChecksDone'}");
+        send.save();
+        send.startBuild().shouldSucceed();
+        send.getLastBuild().getConsole().contains("scott = abcdefg");
+
+        elasticSleep(1000);
+        wait.getLastBuild().shouldSucceed();
+
+    }
+
+    @WithPlugins("workflow-aggregator@1.2")
+    @Test
+    public void testSimpleCIEventTriggerWithPipelineWaitForMsg() throws Exception {
+        WorkflowJob wait = jenkins.jobs.create(WorkflowJob.class);
+        wait.script.set("node('master') {\n def scott = waitForCIMessage  providerName: 'test', " +
+                " selector: " +
+                " \"CI_TYPE = 'code-quality-checks-done' and CI_STATUS = 'failed'\"  \necho \"scott = \" + scott}");
+        wait.save();
+        wait.startBuild();
+
+        FreeStyleJob jobB = jenkins.jobs.create();
+        jobB.configure();
+        CINotifierPostBuildStep notifier = jobB.addPublisher(CINotifierPostBuildStep.class);
+        notifier.messageType.select("CodeQualityChecksDone");
+        notifier.messageProperties.sendKeys("CI_STATUS = failed");
+        notifier.messageContent.set("Hello World");
+        jobB.save();
+        jobB.startBuild().shouldSucceed();
+
+        elasticSleep(1000);
+        wait.getLastBuild().shouldSucceed();
+        assertThat(wait.getLastBuild().getConsole(), containsString("Hello World"));
     }
 
     @Test
